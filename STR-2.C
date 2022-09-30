f44259d77086d1f3fc203e13ea7bb8fa72ca2fa8
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-09-30 13:22:52 +0530

    Add files via upload

---
+++ STR-2.C
@@ -0,0 +1,18 @@
+  //string exa.2
+
+
+  void main()
+  {
+	char s1[10][20];
+	int i;
+	clrscr();
+	for(i=0;i<10;i++)
+	{
+		printf("Enter name-%d:",i+1);
+		scanf("%s",s1[i]);
+	}
+	for(i=0;i<10;i++)
+	{
+		printf("\n name-%d=%s",i+1,s1[i]);
+	}
+  }
---
+++ STRING-1.C
@@ -0,0 +1,16 @@
+//string exa.1
+
+
+void main()
+{
+	char s1[20],s2[20],s3[20];
+	clrscr();
+	puts("Enter a string:");
+	gets(s1);
+	puts("Enter a string:");
+	gets(s2);
+	puts("Enter a string:");
+	gets(s3);
+	printf("\n s1 =%s  \n s2 =%s  \n s3 =%s",s1,s2,s3);
+	getch();
+}
