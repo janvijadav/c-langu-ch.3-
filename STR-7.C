c5dca223a2d18be1e4a305625d3acaf6a9b3bbc9
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-10-07 16:41:20 +0530

    Add files via upload

---
+++ STR-7.C
@@ -0,0 +1,19 @@
+
+
+//string -7
+
+void main()
+{
+	int a,b;
+	clrscr();
+	printf("Enter two nos:");
+	scanf("%d %d",&a,&b);
+	add(a,b);
+	getch();
+}
+ add(int x , int y)
+{
+	int z;
+	z=x+y;
+	printf("\n %d+%d = %d",x,y,z);
+}
